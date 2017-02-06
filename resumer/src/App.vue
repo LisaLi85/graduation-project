d69eb1f127520e9f19b36f24a9a434391d45d6f6
LisaLi85 <756543557@qq.com>
2017-02-06 16:12:20 +0800

    resumer

---
+++ resumer/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class=page>
+     <header>
+       <Topbar/>
+     </header>
+     <main>
+       <ResumeEditor/>
+       <ResumePreview/>
+     </main>
+  </div>
+</template>
+
+<script>
+
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
+
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      text: '你好'
+    }
+  },
+  components: {Topbar,ResumeEditor,ResumePreview}
+}
+</script>
+  
+<style>
+  .page{
+       height: 100vh;
+       display: flex;
+       flex-direction: column;/* 决定项目的排列方向 */
+       background: #EAEBEC;
+  }
+  .page>main{
+       flex-grow: 1; /*属性定义项目的放大比例，默认为0，即如果存在剩余空间，也不放大*/
+  }
+
+  .page>main{
+       min-width: 1024px;
+       max-width: 1440px;
+       margin-top: 16px;
+       margin-bottom: 16px;
+       display: flex;
+       justify-content: space-between;/*属性定义了项目在主轴上的对齐方式,两端对齐，项目之间的间隔都相等*/
+       padding: 0 16px;
+       width: 100%; /* 试试不加这句会怎样 */
+       align-self: center;/*允许单个项目有与其他项目不一样的对齐方式*/
+  }
+
+  #resumeEditor{
+       width: 35%;
+       background: #444;
+  }
+  #resumePreview{
+       /*width: 61.66667%;*/
+       flex-grow: 1;
+       margin-left: 16px;
+       background: #777;
+  }  
+</style>
