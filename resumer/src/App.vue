a0d73115674d22e4e5e55b4794c45d3ad64f4626
LisaLi85 <756543557@qq.com>
2017-02-15 21:03:19 +0800

    resumer

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -11,53 +11,66 @@
 </template>
 
 <script>
-import 'normalize.css/normalize.css'
-import './assets/reset.css'
-import Topbar from './components/Topbar'
-import ResumeEditor from './components/ResumeEditor'
-import ResumePreview from './components/ResumePreview'
-export default {
-  name: 'app',
-  data: function(){
-    return {
-      text: '你好'
-     }
-  },
-  components: {Topbar, ResumeEditor, ResumePreview}
-}
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+  import icons from './assets/icons'
+
+
+  export default {
+    name: 'app',
+    data: function(){
+      return {
+        text: '你好'
+       }
+    },
+    components: { Topbar, ResumeEditor, ResumePreview },
+    created() {
+  	  document.body.insertAdjacentHTML('afterbegin', icons) // 
+    }
+  }
 </script>
 
-<style>
-  .page{
-       height: 100vh;
-       display: flex;
-       flex-direction: column;/* 决定项目的排列方向 */
-       background: #EAEBEC;
-  }
-  .page>main{
-       flex-grow: 1; /*属性定义项目的放大比例，默认为0，即如果存在剩余空间，也不放大*/
-  }
+<style lang="scss">
+  	.page{
+        height: 100vh;
+        display: flex;
+        flex-direction: column;/* 决定项目的排列方向 */
+        background: #EAEBEC;
+  	    >main{
+       		flex-grow: 1; /*属性定义项目的放大比例，默认为0，即如果存在剩余空间，也不放大*/
+  		}
+  		>main{
+	       min-width: 1024px;
+	       max-width: 1440px;
+	       margin-top: 16px;
+	       margin-bottom: 16px;
+	       display: flex;
+	       justify-content: space-between;/*属性定义了项目在主轴上的对齐方式,两端对齐，项目之间的间隔都相等*/
+	       padding: 0 16px;
+	       width: 100%; /* 试试不加这句会怎样 */
+	       align-self: center;/*允许单个项目有与其他项目不一样的对齐方式*/
+	  	}
+	}
 
-  .page>main{
-       min-width: 1024px;
-       max-width: 1440px;
-       margin-top: 16px;
-       margin-bottom: 16px;
-       display: flex;
-       justify-content: space-between;/*属性定义了项目在主轴上的对齐方式,两端对齐，项目之间的间隔都相等*/
-       padding: 0 16px;
-       width: 100%; /* 试试不加这句会怎样 */
-       align-self: center;/*允许单个项目有与其他项目不一样的对齐方式*/
-  }
-
-  #resumeEditor{
-       width: 35%;
-       background: #444;
-  }
-  #resumePreview{
-       /*width: 61.66667%;*/
-       flex-grow: 1;
-       margin-left: 16px;
-       background: #777;
-  }  
+  	#resumeEditor{
+       	   width: 35%;
+           background: #444;
+    }    
+  	#resumePreview{
+           /*width: 61.66667%;*/
+           flex-grow: 1;
+           margin-left: 16px;
+           background: #777;
+    }
+    svg.icon{
+	    height: 1em;
+	    width: 1em;
+	    fill: currentColor;
+	    vertical-align: -0.1em;
+	    font-size:16px;
+    }
 </style>
